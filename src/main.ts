9e7401fef55344af2b03360f341c8608c0445fd6
Philip Andrew Wee De Wang <41712275+PhilipWee@users.noreply.github.com>
2022-08-21 14:15:48 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import startWeeklyPoll from "./startWeeklyPoll";
 import dotenv from "dotenv";
 dotenv.config();
 
+const PORT = process.env.PORT || 3000;
+const URL = process.env.URL;
+
 export async function run() {
   if (!process.env.BOT_TOKEN) {
     throw new Error("Bot token not defined properly!");
@@ -14,7 +17,18 @@ export async function run() {
 
   bot.command("start_weekly_poll", startWeeklyPoll);
 
-  bot.launch();
+  console.log({ PORT, URL });
+
+  if (URL) {
+    //Use webhook when on heroku
+    bot.telegram.setWebhook(`${URL}/bot${process.env.BOT_TOKEN}`);
+    //@ts-ignore
+    bot.startWebhook(`/bot${process.env.BOT_TOKEN}`, null, PORT);
+    console.log("Webhook registered!");
+  } else {
+    bot.launch();
+    console.log("Polling started!");
+  }
 }
 
 run();
